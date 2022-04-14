0dd845f35be781dfa96783d674d4adc07cbbbc08
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-04-14 00:22:23 -0300

    configurado tela de login rj

--- rjcariri/base/views.py
+++ rjcariri/base/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+
+from django.shortcuts import render
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Ola Rj Cariri')
+    return render(request, 'base/home.html')
