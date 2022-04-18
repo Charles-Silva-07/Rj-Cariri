274ba2250f77e5f3c6c846a332005530f4afb667
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-04-18 10:10:22 -0300

    incluido cadastre-se

--- rjcariri/urls.py
+++ rjcariri/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+#from django.urls import include, path
 from rjcariri.base.views import home
 
 urlpatterns = [
