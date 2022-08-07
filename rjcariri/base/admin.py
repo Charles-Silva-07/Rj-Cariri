d6e12db098cb16ccdfcd9893b25ecbf0ffc6268b
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-08-06 21:58:35 -0300

    Criado usuario para Tabela

--- rjcariri/base/admin.py
+++ rjcariri/base/admin.py
@@ -20,7 +20,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
 
-from rjcariri.base.models import User
+from rjcariri.base.models import User, Tabela
 
 csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
@@ -213,3 +213,6 @@ class UserAdmin(admin.ModelAdmin):
             request.POST = request.POST.copy()
             request.POST["_continue"] = 1
         return super().response_add(request, obj, post_url_continue)
+
+
+admin.site.register(Tabela)
